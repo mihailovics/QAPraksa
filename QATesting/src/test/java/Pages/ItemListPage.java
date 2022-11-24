65f787becb24a985658f0febbb4ae7fbf79fa30c
Stevan Mihailovic <mihailovic023@gmail.com>
2022-11-24 17:12:20 +0100

    add one item test

--- QATesting/src/test/java/Pages/ItemListPage.java
+++ QATesting/src/test/java/Pages/ItemListPage.java
@@ -1,4 +1,25 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
 public class ItemListPage {
+    WebDriver driver;
+
+    By addItemButton = By.id("add-to-cart-sauce-labs-backpack");
+    public ItemListPage(WebDriver driver){this.driver = driver;}
+    public void addItemButton(){
+        driver.findElement(addItemButton).click();
+    }
+    public void checkItemAdded(){
+        WebElement cartBadge = driver.findElement(By.className("shopping_cart_badge"));
+        Assert.assertEquals(cartBadge,driver.findElement(By.className("shopping_cart_badge")));
+    }
+
+
+
+
+
 }
--- QATesting/src/test/java/runners/ItemPages/ItemListStandardUserTest.java
+++ QATesting/src/test/java/runners/ItemPages/ItemListStandardUserTest.java
@@ -1,9 +1,44 @@
 package runners.ItemPages;
 
+import Pages.ItemListPage;
+import Pages.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class ItemListStandardUserTest {
+    //login
+    //add to cart
+    WebDriver driver = new ChromeDriver();
+    LoginPage loginPage = new LoginPage(driver);
+    final static String loginPageUrl="https://saucedemo.com";
+    ItemListPage itemListPage = new ItemListPage(driver);
+
+    @BeforeClass
+    public void setupClass(){
+        String loginPage = loginPageUrl;
+        driver.get(loginPage);
+    }
+    @BeforeMethod
+    private void refreshPage(){
+        driver.get(loginPageUrl);
+    }
+
+    public void loginUser(){
+        loginPage.loginToSauceDemo("standard_user","secret_sauce");
+    }
+    @Test
+    public void addItem(){
+        loginUser();
+        itemListPage.addItemButton();
+        itemListPage.checkItemAdded();
+
+
+    }
 }
-//login
-//add to cart
+
 
 //login
 //add to cart vise itema
